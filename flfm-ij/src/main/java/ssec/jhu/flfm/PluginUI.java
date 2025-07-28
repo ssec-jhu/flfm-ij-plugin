c606d5a54402b49d26e51cf9344cc8518e5bf113
Ryan Hausen <ryan.hausen@gmail.com>
2025-07-28 08:35:19 -0400

    added shade plugin to the profiles to the build for windows/linux, combined standalone/pluginUI

--- flfm-ij/src/main/java/ssec/jhu/flfm/PluginUI.java
+++ flfm-ij/src/main/java/ssec/jhu/flfm/PluginUI.java
@@ -1,5 +1,7 @@
 package ssec.jhu.flfm;
 
+import javax.swing.SwingUtilities;
+
 import ij.gui.*;
 import ij.plugin.frame.*;
 
@@ -9,10 +11,24 @@ public class PluginUI extends PlugInFrame {
     super("PluginUI");
   }
 
+  public PluginUI(String title) {
+    super(title);
+  }
+
   public void run(String arg) {
     add(new SharedPanel());
     pack();
     GUI.center(this);
     setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      () -> {
+        PluginUI ui = new PluginUI("Standalone UI");
+        ui.pack();
+        ui.setVisible(true);
+        ui.setLocationRelativeTo(null);
+    });
   }
 }
